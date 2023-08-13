79cd4659cff1da782c0f418decc8bf1e1e293711
Surav Shrestha <surav_02@live.com>
2023-08-13 18:34:30 +0545

    feat(database): set mongodb uri based on environment

--- utils/config.js
+++ utils/config.js
@@ -7,10 +7,17 @@ const SMTP_PORT = process.env.SMTP_PORT || 587;
 const SMTP_EMAIL = process.env.SMTP_EMAIL;
 const SMTP_PASSWORD = process.env.SMTP_PASSWORD;
 
-const MONGODB_URI =
-  process.env.NODE_ENV === "test"
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/logtracker";
+// Set the MongoDB URI based on the current environment
+let MONGODB_URI;
+if (process.env.NODE_ENV === "production") {
+  MONGODB_URI = process.env.PRODUCTION_MONGODB_URI;
+} else if (process.env.NODE_ENV === "test") {
+  MONGODB_URI = process.env.TEST_MONGODB_URI;
+} else {
+  MONGODB_URI =
+    process.env.DEVELOPMENT_MONGODB_URI ||
+    "mongodb://0.0.0.0:27017/logtracker";
+}
 
 const SECRET = process.env.SECRET;
 
