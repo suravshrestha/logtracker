94346e2d0425ffe90c5b5f4ce8783c9c2a97946d
Surav Shrestha <surav_02@live.com>
2023-08-13 21:19:25 +0545

    feat(database): sync students of 4 batches from the newest batch

--- utils/batches.js
+++ utils/batches.js
@@ -1,3 +1,10 @@
-const batches = ["075", "076", "077", "078"];
+const NepaliDate = require("nepali-date-converter");
+const nepDate = new NepaliDate();
+
+let recentBatch = nepDate.getYear() - 1;
+let batches = [];
+for (let i = 0; i < 4; ++i) {
+  batches.push(String(recentBatch - i).slice(1, 4));
+}
 
 module.exports = batches;
